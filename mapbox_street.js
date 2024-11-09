4d0f9118d43d460f8cfa200fb8e232538af7694a
calebyhan <calebhantech@gmail.com>
2024-11-09 14:36:56 -0500

    commit

--- mapbox_street.js
+++ mapbox_street.js
@@ -1,17 +1,14 @@
+// Get the map element
+const mapElement = document.querySelector('.map');
+
+// Retrieve latitude and longitude from data attributes
+const latitude = mapElement.getAttribute('data-lat');
+const longitude = mapElement.getAttribute('data-lng');
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsZWJoYW4iLCJhIjoiY20zYWZhemx6MTJvMDJqcHhuOHpzMzIxaCJ9.Ex5-2rGapj9t0afOM2s4Yw';
 const map = new mapboxgl.Map({
-    container: 'map',
-    center: [-122.420679, 37.772537],
-    zoom: 13,
-    style: 'mapbox://styles/mapbox/standard',
-    config: {
-        basemap: {
-            lightPreset: 'night'
-        }
-    }
+    container: mapElement,
+    center: [longitude, latitude],
+    zoom: 16,
+    pitch: 62,
 });
-
-map.on('style.load', () => {
-
-
-})
